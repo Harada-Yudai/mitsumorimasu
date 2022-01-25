4bf212b122651f475e52eb45a88a375f46a0f025
Harada-Yudai <y.harada.dm@gmail.com>
2022-01-25 23:45:12 +0900

    ユーザー機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
-
-
+  private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_kana, :first_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_kana, :first_kana, :company, :address, :phone_num,:info])
   end
 
 
