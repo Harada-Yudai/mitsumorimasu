b86215160ce10e7646f48cc18bd937dce20912c6
Harada-Yudai <y.harada.dm@gmail.com>
2022-02-11 22:10:15 +0900

    readme準備

---
+++ db/migrate/20220209124503_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item_name           , null: false
+      t.string :item_price          , null: false
+      t.string :item_amount         , null: false
+      t.text   :item_info 
+      t.references :project            , null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
